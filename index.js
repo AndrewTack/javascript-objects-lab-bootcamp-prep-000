433a08ea62776119ae231717d1046a0ab1911253
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-29 15:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   let newObj = Object.assign({}, object)
   delete newObj.key
-  return object
+  return newObj
 }
